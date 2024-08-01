fd273a60e06f4a0ffcdf8b713306177dcbae56e4
Joshua John <joshuabaridakarajohn@gmail.com>
2024-08-02 00:24:24 +0100

    Ngozie

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* typing animation*/
+var typed = new Typed(".typing", {
+    strings:["","Surgeon", "Public Speaker", "Anime-Lover", "Chef"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true 
+}) 
